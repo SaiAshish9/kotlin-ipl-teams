50d561c4742c5593f7bd087a3afbf0a453fb6e1c
saiashish9 <saiashish3760@gmail.com>
2021-02-21 02:17:01 +0530

    initial commit

---
+++ app/src/main/java/com/example/kotliniplteams/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kotliniplteams
+
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}

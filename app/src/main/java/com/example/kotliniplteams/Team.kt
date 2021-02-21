b2ab87f0ee2d9fdb876cba190152ff05b440dd47
saiashish9 <saiashish3760@gmail.com>
2021-02-21 14:18:13 +0530

    team model updated

---
+++ app/src/main/java/com/example/kotliniplteams/Team.kt
@@ -0,0 +1,9 @@
+package com.example.kotliniplteams
+
+class Team(var name:String?=null,var desc:String?=null,var image:Int?=null) {
+
+//    constructor(name:String,desc:String,image:Int){
+//
+//    }
+
+}
